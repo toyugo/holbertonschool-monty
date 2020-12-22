a187e1045ca6ac889cbed79ff3344b228009575d
Olivier <olivier.guyot.cu@gmail.com>
2020-12-22 20:34:42 +0000

    Fix

---
+++ pint.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * f_pint - f_pint
+ * @stack: stack
+ * @line_number: line number
+ * Return: tab of char
+ */
+void f_pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr;
+
+	ptr = *stack;
+	if (ptr->next == NULL)
+	{
+		ERR_pint(line_number);
+		return;
+	}
+	printf("%d\n", (*stack)->next->n);
+}
