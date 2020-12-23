ae0b8920a5742d9c8caaa99ab94c686eff562027
Olivier <olivier.guyot.cu@gmail.com>
2020-12-23 20:42:41 +0000

    Fix

--- op_function_str.c
+++ op_function_str.c
@@ -31,8 +31,8 @@ void f_pchar(stack_t **stack, unsigned int line_number)
 void f_pstr(stack_t **stack, unsigned int line_number)
 {
 	stack_t *ptr;
-	ptr = *stack;
 
+	ptr = *stack;
 	if (ptr == NULL)
 	{
 		ERR_pchar(line_number);
